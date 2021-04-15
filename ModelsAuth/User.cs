c645e173d07fbfb09e676a8f17a2fb49a84cf012
DethleSS <rik546@mail.ru>
2021-04-15 02:11:21 -0700

    fix: Models

--- ModelsAuth/User.cs
+++ ModelsAuth/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,20 @@ namespace WebMovie.ModelsAuth
 {
     public class User
     {
+        [Required]
+        public Guid UId { get; set; }
+        [Required]
+        public string UEmail { get; set; }
+        [Required]
+        public int UPassword { get; set; }
+        [Required]
+        public string UFirstName { get; set; }
+        [Required]
+        public string USecondName { get; set; }
+        [Required]
+        public string UUserName { get; set; }
+        [Required]
+        public int URole { get; set; }
+
     }
 }
--- ModelsAuth/UserRole.cs
+++ ModelsAuth/UserRole.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,12 @@ namespace WebMovie.ModelsAuth
 {
     public class UserRole
     {
+        [Required]
+        public Guid URId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public int RoleId { get; set; }
     }
 }
